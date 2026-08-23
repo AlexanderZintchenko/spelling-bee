7bc5da438ff9b1e2d1e0647dcdb00e96846ae7cc
AlexanderZintchenko <sazintchenko@googlemail.com>
2026-08-23 04:44:58 +0200

    Add mode selection and basic xp mode

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import penguin from "./assets/penguin.webp";
 import astolfo from "./assets/astolfo_bday.webp";
 import "./App.css";
 import english5k from "./static/english_5k.json";
-import * as speech from "./utils/speech.ts"
+import * as speech from "./utils/speech.ts";
 
 const preload = new Image();
 preload.src = astolfo;
@@ -23,6 +23,8 @@ const DICTIONARY_IDS = {
   GERMAN_10K: "4",
 } as const;
 
+type DictionaryId = (typeof DICTIONARY_IDS)[keyof typeof DICTIONARY_IDS];
+
 const DICTIONARIES = {
   "0": () => import("./static/english_5k.json"),
   "1": () => import("./static/english_10k.json"),
@@ -30,6 +32,21 @@ const DICTIONARIES = {
   "3": () => import("./static/english_shakespearean.json"),
   "4": () => import("./static/german_10k.json"),
 };
+
+const XP_MODIFIERS = {
+  [DICTIONARY_IDS.ENGLISH_5K]: 1,
+  [DICTIONARY_IDS.ENGLISH_10K]: 1.25,
+  [DICTIONARY_IDS.ENGLISH_25K]: 1.5,
+  [DICTIONARY_IDS.SHAKESPEAREAN]: 2,
+  [DICTIONARY_IDS.GERMAN_10K]: 1,
+} as const;
+
+const MODES = {
+  DEFAULT: "classic",
+  XP: "XP",
+} as const;
+
+type Mode = (typeof MODES)[keyof typeof MODES];
 
 /**
  * Store react state in localStorage so they persist across page reloads.
@@ -62,8 +79,9 @@ function useLocalStorage<T>(key: string, initialValue: T) {
 function initializeVoice(voices: SpeechSynthesisVoice[]) {
   console.log(voices);
   return (
-    speech.PREFERRED_VOICES.map((name) => voices.find((voice) => voice.name === name)).find((voice) => voice !== undefined) ??
-    voices[0]
+    speech.PREFERRED_VOICES.map((name) => voices.find((voice) => voice.name === name)).find(
+      (voice) => voice !== undefined,
+    ) ?? voices[0]
   );
 }
 
@@ -102,13 +120,24 @@ function App() {
   const [falseCounter, setFalseCounter] = useLocalStorage("falseCounter", 0);
   const [lastFalseWord, setLastFalseWord] = useLocalStorage("lastFalseWord", "-");
   const [logo, setLogo] = useState(penguin);
+  const [mode, setMode] = useState<Mode>(MODES.DEFAULT);
   const [openSettings, setOpenSettings] = useState(false);
   const [volume, setVolume] = useState(DEFAULT_SETTINGS.volume);
   const [pitch, setPitch] = useState(DEFAULT_SETTINGS.pitch);
   const [rate, setRate] = useState(DEFAULT_SETTINGS.rate);
   const [dictionary, setDictionary] = useState<Dictionary>(english5k);
-  const [dictionaryId, setDictionaryId] = useState<string>(DICTIONARY_IDS.ENGLISH_5K);
+  const [dictionaryId, setDictionaryId] = useState<DictionaryId>(DICTIONARY_IDS.ENGLISH_5K);
   const [definition, setDefinition] = useState("");
+
+  const [xp, setXp] = useLocalStorage("xp", 0);
+  const level = Math.floor(xp / 50) + 1;
+
+  function calculateXp() {
+    const baseXp = 10;
+    const xp = Math.round(baseXp * XP_MODIFIERS[dictionaryId]);
+
+    return xp;
+  }
 
   function getRandomWord() {
     const randomIndex = Math.floor(Math.random() * dictionary.words.length);
@@ -145,13 +174,18 @@ function App() {
    */
   function generateRandomWord() {
     if (wordClass === "text-input-correct") {
-      const newStreak = streakCounter + 1;
+      if (mode === MODES.DEFAULT) {
+        const newStreak = streakCounter + 1;
 
-      setStreakCounter(newStreak);
-      setCorrectCounter((current) => current + 1);
+        setStreakCounter(newStreak);
+        setCorrectCounter((current) => current + 1);
 
-      if (newStreak > bestStreakCounter) {
-        setBestStreakCounter(newStreak);
+        if (newStreak > bestStreakCounter) {
+          setBestStreakCounter(newStreak);
+        }
+      } else if (mode === MODES.XP) {
+        const wordXp = calculateXp();
+        setXp((current) => current + wordXp);
       }
     } else {
       setStreakCounter(0);
@@ -173,7 +207,7 @@ function App() {
   }
 
   /* Import DICTIONARY corresponding to given DICTIONARY_ID */
-  async function handleDictionaryChange(id: string) {
+  async function handleDictionaryChange(id: DictionaryId) {
     setDictionaryId(id);
 
     const loadDictionary = DICTIONARIES[id as keyof typeof DICTIONARIES];
@@ -198,57 +232,100 @@ function App() {
           </button>
         </div>
       </header>
-      <div className="flex row">
-        <div className="left">
-          <div className="definition-box">
-            <p>{definition}</p>
+      <div className="flex column main-content-container">
+        <div className="top-content">
+          <div className="top-content-left">
+            <div className="definition-box">
+              <p>{definition}</p>
+            </div>
+          </div>
+          <div className="top-content-middle">
+            <div className="hero flex column no-gap">
+              <img src={logo} id="logo" className="logo" alt="anime peace sign" />
+              <div className="main-heading-container">
+                <h1 className="main-heading">Spelling Bee</h1>
+                <img className="heading-bee" src={"bee.svg"} />
+              </div>
+            </div>
+            <div className="flex column container">
+              <p className={wordClass}>{randomWord}</p>
+              <button type="button" className="generator" onClick={() => generateRandomWord()}>
+                Generate random word
+              </button>
+              <input
+                ref={inputRef}
+                className={"text-input " + (matching ? "matching" : "not-matching")}
+                value={textInput}
+                onChange={(event) => {
+                  const value = event.target.value;
+                  setTextInput(value);
+                  const lowerCasedValue = value.toLowerCase();
+                  if (lowerCasedValue === "astolfo") {
+                    setLogo(astolfo);
+                  }
+                }}
+                onKeyDown={(event) => {
+                  if (event.key === "Enter") {
+                    generateRandomWord();
+                  } else if (event.key === "Control") {
+                    wordToSpeech(randomWord);
+                  }
+                }}
+              />
+              <button type="button" className="repeat-button" onClick={() => wordToSpeech(randomWord)}>
+                🕪
+              </button>
+            </div>
+          </div>
+          <div className="top-content-right">
+            {mode === MODES.XP && (
+              <div>
+                <p>Level: {level}</p>
+                <p>XP to next level: {50 - (xp % 50)}</p>
+              </div>
+            )}
           </div>
         </div>
-        <div className="main-content">
-          <div className="hero flex column less-gap">
-            <img src={logo} id="logo" className="logo" alt="anime peace sign" />
-            <h1 >Spelling Bee</h1>
-          </div>
-          <div className="flex column container">
-            <p className={wordClass}>{randomWord}</p>
-            <button type="button" className="generator" onClick={() => generateRandomWord()}>
-              Generate random word
-            </button>
-            <input
-              ref={inputRef}
-              className={"text-input " + (matching ? "matching" : "not-matching")}
-              value={textInput}
-              onChange={(event) => {
-                const value = event.target.value;
-                setTextInput(value);
-                const lowerCasedValue = value.toLowerCase();
-                if (lowerCasedValue === "astolfo") {
-                  setLogo(astolfo);
-                }
-              }}
-              onKeyDown={(event) => {
-                if (event.key === "Enter") {
-                  generateRandomWord();
-                } else if (event.key === "Control") {
-                  wordToSpeech(randomWord);
-                }
-              }}
-            />
-            <button type="button" className="repeat-button" onClick={() => wordToSpeech(randomWord)}>
-              🕪
-            </button>
-            <p className="streakCounter">streak: {streakCounter}</p>
-            <p className="streakCounter">best streak: {bestStreakCounter}</p>
-            <p className="streakCounter">correct: {correctCounter}</p>
-            <p className="streakCounter">false: {falseCounter}</p>
-            <p>
-              last false word:
-              <br />
-              {lastFalseWord}
-            </p>
-          </div>
+        <div className="stats-container">
+          {mode === MODES.DEFAULT && (
+            <div className="stats">
+              <p className="streakCounter">streak: {streakCounter}</p>
+              <p className="streakCounter">best streak: {bestStreakCounter}</p>
+              <p className="streakCounter">correct: {correctCounter}</p>
+              <p className="streakCounter">false: {falseCounter}</p>
+              <p>
+                last false word:
+                <br />
+                {lastFalseWord}
+              </p>
+            </div>
+          )}
+
+          {mode === MODES.XP && (
+            <div className="stats-xp">
+              <select
+                className="select"
+                id="difficulty"
+                value={dictionaryId}
+                onChange={(event) => handleDictionaryChange(event.target.value as DictionaryId)}
+              >
+                <option value={DICTIONARY_IDS.ENGLISH_5K}>Beginner</option>
+                <option value={DICTIONARY_IDS.ENGLISH_10K}>Medium (1.25XP)</option>
+                <option value={DICTIONARY_IDS.ENGLISH_25K}>Advanced (1.5XP)</option>
+                <option value={DICTIONARY_IDS.SHAKESPEAREAN}>Hard (2XP)</option>
+              </select>
+            </div>
+          )}
         </div>
-        <div className="right"></div>
+        <select
+          className="select"
+          id="select-mode"
+          value={mode}
+          onChange={(event) => setMode(event.target.value as Mode)}
+        >
+          <option value={MODES.DEFAULT}>Classic Mode</option>
+          <option value={MODES.XP}>XP Mode</option>
+        </select>
       </div>
       {openSettings && (
         <div className="overlay-backdrop" onClick={() => setOpenSettings(false)}>
@@ -330,7 +407,7 @@ function App() {
                 <select
                   id="dictionaries"
                   value={dictionaryId}
-                  onChange={(event) => handleDictionaryChange(event.target.value)}
+                  onChange={(event) => handleDictionaryChange(event.target.value as DictionaryId)}
                 >
                   <option value={DICTIONARY_IDS.ENGLISH_5K}>English - 5k</option>
                   <option value={DICTIONARY_IDS.ENGLISH_10K}>English - 10k</option>
